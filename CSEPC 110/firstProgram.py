6d618bb1dbc973aadc4ddeeb1cdd2f1d72c8e354
Webenedog <34722056+brunogcs@users.noreply.guthub.com>
2025-05-06 15:06:27 -0700

    first commit

---
+++ CSEPC 110/firstProgram.py
@@ -0,0 +1,13 @@
+print("Hello World")
+print("")
+print("Welcome to CSEPC110")
+name = "Bob"
+age = 28
+print(name + '' + str(age))
+name = "Junior"
+age = 30
+print(name + '' + str(age))
+print(f"Your name is {name} and your age is {age}")
+
+local = input("Where are you from? ")
+print(f"You are from {local}")
