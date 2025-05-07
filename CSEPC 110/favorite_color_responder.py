8158528eea78315e4bccc408124b80220385bc46
Webenedog <34722056+brunogcs@users.noreply.guthub.com>
2025-05-07 14:04:36 -0700

    new week 1 programs

---
+++ CSEPC 110/favorite_color_responder.py
@@ -0,0 +1,3 @@
+user_favorite_color = input("What is your favorite color? ")
+answer = (f"Your favorite color is \n{user_favorite_color}")
+print(answer)
---
+++ CSEPC 110/name.py
@@ -0,0 +1,4 @@
+first_name = input('What is your first name? ')
+last_name = input('What is your last name? ')
+full_name = (first_name.capitalize()+' '+last_name.capitalize())
+print(f"Your name is {last_name.capitalize()}, {full_name}")
