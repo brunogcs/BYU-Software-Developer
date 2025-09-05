bbad1e68e382e05d55b7a9e30e0f018698d3b176
Bruno Gabriel <34722056+brunogcs@users.noreply.github.com>
2025-09-05 12:13:43 -0300

    update week01 cse111

--- CSE 111/week01/tire_volume.py
+++ CSE 111/week01/tire_volume.py
@@ -1,16 +1,32 @@
 import math
+import datetime
 
-#get user input
-width_mm            = float(input('Enter the width of the tire in mm (ex: 205): '))
-aspect_ratio        = float(input('Enter the aspect ratio of the tire (ex: 60): '))
-wheel_diameter_inch = float(input('Enter the diameter of the wheel in inches (ex: 15): '))
+#get user input (correct answer: 205, 60, 15 = 39.92)
+width_mm            = int(input('Enter the width of the tire in mm (ex: 205): '))
+aspect_ratio        = int(input('Enter the aspect ratio of the tire (ex: 60): '))
+wheel_diameter_inch = int(input('Enter the diameter of the wheel in inches (ex: 15): '))
 
-#compute volume liters
-width_mm_squared = width_mm ** 2
-avg_circumference_tire = (width_mm * aspect_ratio + 2540 * wheel_diameter_inch)
-area_base = math.pi * width_mm_squared
-profile_area =  area_base * aspect_ratio
-raw_volume_mm = profile_area * avg_circumference_tire
-volume_liters = raw_volume_mm / 10_000_000_000
+def compute_volume_liters():
 
-print(f"The approximate volume is {volume_liters:.2f} liters.")
+    #compute volume liters
+    width_mm_squared = width_mm ** 2
+    avg_circumference_tire = (width_mm * aspect_ratio + 2540 * wheel_diameter_inch)
+    area_base = math.pi * width_mm_squared
+    profile_area =  area_base * aspect_ratio
+    raw_volume_mm = profile_area * avg_circumference_tire
+    volume_liters = raw_volume_mm / 10_000_000_000
+
+    return format(volume_liters, ".2f")
+
+def log_volume_liters(width_mm, aspect_ratio, wheel_diameter_inch, volume_liters):
+
+    current_date= str(datetime.datetime.now().date())
+    
+    # Open a text file in append mode and insert the tire volume information
+    with open("tire_volume.txt", "a") as tire_volume_file:
+        print(current_date + ', ' + str(width_mm) + ', ' + str(aspect_ratio) + ', ' + str(wheel_diameter_inch) + ', ' + str(volume_liters), file=tire_volume_file)
+
+if __name__ == "__main__":
+    volume_liters = compute_volume_liters()
+    print(f"The approximate volume is {volume_liters} liters.")
+    log_volume_liters(width_mm, aspect_ratio, wheel_diameter_inch, volume_liters)
